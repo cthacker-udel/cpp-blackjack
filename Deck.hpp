05a4c1758e10ffec8e2bfa4d5237526cf122d317
Cameron Thacker <cthacker@udel.edu>
2021-12-28 23:53:43 -0500

    Added description of Deck class

--- Deck.hpp
+++ Deck.hpp
@@ -3,6 +3,29 @@
 
 class Deck {
 
+    /*
+
+    Deck class
+
+    private attributes:
+    Card pointer - cards within deck
+    numCards - integer : number of cards left
+
+    public attributes:
+        no-arg constructor
+        void deal() - returns Card * <-- card that is dealt
+        void shuffle() - shuffles deck
+        Deck(Card *) <-- intializes deck with empty pointer
+        void addCard(Card *) <--- adds card to deck
+        ~Deck() - Deck destructor
+        void printDeck() <--- prints deck
+        int getNumCards() <-- returns number of cards in deck
+        void setNumCards() <-- sets number of cards in deck 
+        void displayHandTotal <-- displays hand total given string which is play name
+        int getTotal <-- gets total rank of hand
+
+    */
+
     Card *cards;
     int numCards = 0;
     friend class Player;
